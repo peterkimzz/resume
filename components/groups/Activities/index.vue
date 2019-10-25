a13d82cce857ceb13a237fe81364097798dd5367
Peter Kim <peterkimzz69@gmail.com>
2019-10-26 08:04:57 +0900

    activities

--- components/groups/Activities/index.vue
+++ components/groups/Activities/index.vue
@@ -7,20 +7,20 @@
         block
         sub-title="YouTube Channel"
         title="유튜브 채널">
-        코딩 강의 및 취미 생활을 직접 동영상 촬영하고 편집해서 종종 업로드 하고 있습니다.
-      </vue-definition>
-      <vue-definition
-        block
-        sub-title="class101"
-        title="클래스101">
-        주로 다루는 코딩, 프로그래밍 기술에 대한 온라인 강의를 준비 중입니다.
+        코딩 강의 및 취미 생활을 촬영하고 편집해서 종종 업로드 하고 있습니다.
       </vue-definition>
       <vue-definition
         block
         sub-title="Talling"
         title="탈잉"
         href="https://taling.me/Talent/Detail/17455">
-        주로 다루는 코딩, 프로그래밍 기술에 대한 소규모 오프라인 강의를 진행 중입니다.
+        코딩, 프로그래밍 기술에 대한 소규모 오프라인 강의를 진행 중입니다.
+      </vue-definition>
+      <vue-definition
+        block
+        sub-title="class101"
+        title="클래스101">
+        코딩, 프로그래밍 기술에 대한 온라인 강의를 준비 중입니다.
       </vue-definition>
     </div>
   </vue-title-container>
